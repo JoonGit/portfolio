3dbc00e4be61b576d16aab888c9f4dbf5ec04f6d
JoonGit <ljj9730@gmail.com>
2022-07-07 23:28:18 +0900

    Portfolio_Creation_Day4_(?)

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -1,7 +1,10 @@
 let navList = ["글쓰기", "게시판", "마이페이지"];
 let srcList = ["/main/create_plan", "#", "/main/mypage"];
-let list = "";
+let list = `<ul class="nav justify-content-end">`;
 for (let i = 0; i < navList.length; i++) {
-  list += `<li><a href=${srcList[i]}>${navList[i]}</a></li>`;
+  list += `<li class="nav-item">
+  <a class="nav-link active" aria-current="page" href=${srcList[i]}>${navList[i]}</a>
+</li>`;
 }
+list += `</ul>`;
 document.write(list);
